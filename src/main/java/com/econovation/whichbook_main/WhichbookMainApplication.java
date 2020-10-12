a86ac9604d5a9acb56d002aa5d72886353b91e69
UpperLeaf <tkdduq8260@gmail.com>
2020-10-12 12:04:07 +0900

    Init : Which Book Main Web Service

---
+++ src/main/java/com/econovation/whichbook_main/WhichbookMainApplication.java
@@ -0,0 +1,13 @@
+package com.econovation.whichbook_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhichbookMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhichbookMainApplication.class, args);
+    }
+
+}
